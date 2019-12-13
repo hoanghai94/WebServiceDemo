2a70fed72bf1a99a5fcfc82bb6fc8a53b66a43a7
hai <hoanghai@gmail.com>
2019-12-13 20:13:26 +0700

    commit

---
+++ WcfServiceDemo/Book.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WcfServiceDemo
+{
+    [DataContract]
+    public class Book
+    {
+        [DataMember]
+        public int BookId { get; set; }
+        [DataMember]
+        public string Title { get; set; }
+        [DataMember]
+        public string ISBN { get; set; }
+    }
+
+    public interface IBookRepository
+    {
+        List<Book> GetAllBooks();
+        Book GetBookById(int id);
+        Book AddNewBook(Book item);
+        bool DeleteABook(int id);
+        bool UpdateABook(Book item);
+    }
+
+    public class BookRepository : IBookRepository
+    {
+        private List<Book> books = new List<Book>();
+        private int counter = 1;
+
+        public BookRepository()
+        {
+            AddNewBook(new Book { Title = "C# Programming", ISBN = "123" });
+            AddNewBook(new Book { Title = "Java Programming", ISBN = "456" });
+            AddNewBook(new Book { Title = "Wfc Programming", ISBN = "789" });
+        }
+
+        public Book AddNewBook(Book newBook)
+        {
+            if (newBook == null)
+                throw new ArgumentNullException("newBook");
+            newBook.BookId = counter++;
+            books.Add(newBook);
+            return newBook;
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return books;
+        }
+
+        public Book GetBookById(int bookId)
+        {
+            return books.Find(b => b.BookId == bookId);
+        }
+
+        public bool UpdateABook(Book updatedBook)
+        {
+            if (updatedBook == null)
+                throw new ArgumentNullException("updateBook");
+
+            int idx = books.FindIndex(b => b.BookId == updatedBook.BookId);
+            if (idx == -1)
+                return false;
+            books.RemoveAt(idx);
+            books.Add(updatedBook);
+            return true;
+        }
+
+        public bool DeleteABook(int bookId)
+        {
+            int idx = books.FindIndex(b => b.BookId == bookId);
+            if (idx == -1)
+                return false;
+            books.RemoveAll(b => b.BookId == bookId);
+            return true;
+        }
+    }
+
+    
+}
---
+++ WcfServiceDemo/BookServices.svc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceDemo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BookServices" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select BookServices.svc or BookServices.svc.cs at the Solution Explorer and start debugging.
+    public class BookServices : IBookServices
+    {
+        static IBookRepository repository = new BookRepository();
+        public string AddBook(Book book, string id)
+        {
+            Book newBook = repository.AddNewBook(book);
+            return "id=" + newBook.BookId;
+        }
+
+        public string DeleteBook(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoWork()
+        {
+        }
+
+        public Book GetBookById(string id)
+        {
+            return repository.GetBookById(int.Parse(id));
+        }
+
+        public List<Book> GetBookList()
+        {
+            return repository.GetAllBooks();
+        }
+
+        public string UpdateBook(Book book, string id)
+        {
+            bool updated = repository.UpdateABook(book);
+            if (updated)
+                return "Book with id = " + id + " update success";
+            else
+                return "Unable to detele book with id = " + id;
+        }
+    }
+}
